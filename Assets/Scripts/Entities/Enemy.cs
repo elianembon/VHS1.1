8c0436e83aa8fa5644815584919009e066896275
Owwly <luciachilievich@gmail.com>
2023-09-26 03:20:50 -0300

    rango de deteccion enemigo
    ahora el enemigo persigue al jugador dependiendo de que tan cerca este de el

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public PlayerManager sacarVida;
+    public Transform jugador; //ref jugador
+    public float rangoPersecusion = 10.0f;
+    public float velocidad = 5.0f;
+    Rigidbody2D rb;
 
-    private void Start()
+ 
+private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         sacarVida = GameObject.FindObjectOfType<PlayerManager>();
     }
     void OnCollisionEnter2D(Collision2D collision)
@@ -18,6 +25,23 @@ public class Enemy : MonoBehaviour
         {
             sacarVida.LooseLife();        
             
+        }
+        
+        
+        
+    }
+
+    private void Update()
+    {
+        float distancia = Vector3.Distance(transform.position, jugador.position); //calcula distancia entre enemigo y jugador
+
+        if (distancia <= rangoPersecusion)
+        {
+            //calcula direccion hacia el jugador
+            Vector3 direccion = (jugador.position - transform.position).normalized;
+            
+            //mueve enemigo hacia el jugador
+           rb.velocity = direccion * (velocidad * Time.deltaTime);
         }
     }
 }
