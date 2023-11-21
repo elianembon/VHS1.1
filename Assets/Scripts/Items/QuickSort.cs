eb6edc2e7df45435fd8f8ddcf965c8671d941aa4
matilong16 <matilong2019@gmail.com>
2023-11-21 13:54:41 -0300

    Revert "Revert "Quicksort""
    This reverts commit 0897ae580da0cbd806e2c5161b2df8015a8adf01.

--- Assets/Scripts/Items/QuickSort.cs
+++ Assets/Scripts/Items/QuickSort.cs
@@ -1,30 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 
 
-public static class Quicksort
+
+public static class Quicksort<T>
 {
-    public static void Sort(List<GameObject> list, int left, int right)
+    public static void Sort(List<T> list, Comparison<T> comparison, int left, int right)
     {
         if (left < right)
         {
-            int pivotIndex = Partition(list, left, right);
-            Sort(list, left, pivotIndex - 1);
-            Sort(list, pivotIndex + 1, right);
+            int pivotIndex = Partition(list, comparison, left, right);
+            Sort(list, comparison, left, pivotIndex - 1);
+            Sort(list, comparison, pivotIndex + 1, right);
         }
     }
 
-    private static int Partition(List<GameObject> list, int left, int right)
+    private static int Partition(List<T> list, Comparison<T> comparison, int left, int right)
     {
-        int pivotValue = 2; // Valor correspondiente a "Medic"
+        int pivotIndex = (left + right) / 2; // Usar el índice central como pivote
+        T pivotValue = list[pivotIndex];
+        Swap(list, pivotIndex, right);
+
         int i = left - 1;
 
         for (int j = left; j < right; j++)
         {
-            int currentTagValue = GetTagValue(list[j].tag);
-            if (currentTagValue < pivotValue)
+            if (comparison(list[j], pivotValue) < 0)
             {
                 i++;
                 Swap(list, i, j);
@@ -35,26 +39,10 @@ public static class Quicksort
         return i + 1;
     }
 
-    private static void Swap(List<GameObject> list, int i, int j)
+    private static void Swap(List<T> list, int i, int j)
     {
-        GameObject temp = list[i];
+        T temp = list[i];
         list[i] = list[j];
         list[j] = temp;
-    }
-
-    private static int GetTagValue(string tag)
-    {
-        // Asigna valores numéricos a las etiquetas
-        switch (tag)
-        {
-            case "Medic":
-                return 2;
-            case "Item":
-                return 1;
-            case "Objects":
-                return 3;
-            default:
-                return 0; // Valor predeterminado en caso de etiqueta desconocida
-        }
     }
 }
